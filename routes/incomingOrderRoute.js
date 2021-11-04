e1e4aa96e37bd4822f8f343aa730952862622f6d
uhakdt <uhakdt@gmail.com>
2021-11-04 13:28:42 +0000

    Now Takes in and sends order info to kaientai-api.

--- routes/incomingOrderRoute.js
+++ routes/incomingOrderRoute.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const request = require('request');
 
 const router = express.Router();
 
+let currentURL = process.env.KAIENTAI_API_URL_PROD;
+
+if(process.env.NODE_ENV === 'Development') {
+  currentURL = process.env.KAIENTAI_API_URL_DEV
+}
 // GET INCOMING ORDER DETAILS
 router.post('/api/v1/sendOrder', async (req, res) => {
+
+  const reqOptKLFWooCommerce = {
+    url: `${currentURL}/api/v1/klf/woocommerce/${2}`,
+    method: 'POST',
+    json: {
+      "dataBody": req.body
+    },
+  };
+
+  request(reqOptKLFWooCommerce, (err, resKLFWooCommerce, body) => {
+    console.log(err)
+    console.log(body)
+  })
+
   try {
     console.log(req.body)
+
     res.status(200).json({
       status: "OK",
       data: {
