c85783d9d05cfa16afccf1a0d9194f6c6fa18b6e
uhakdt <60895762+uhakdt@users.noreply.github.com>
2021-12-08 14:50:53 +0000

    ignore.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const app = require('./app');
+// const { WooCommerceAPISetup } = require('./WooCommerceAPI/setup');
 
 const port = process.env.PORT || 9967;
 
 app.get('/', (req, res) => {
   res.send("I am going to become the King of the Pirates. If this means I will die on the journey, so be it!");
 });
+
+// WooCommerceAPISetup();
 
 app.listen(port, () => {
   console.log(`Server is running... Listening on port ${port}`);
